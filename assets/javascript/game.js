e12a7ee7c024b52c321a59697a48246cfcd88836
Mike M <mikal815@users.noreply.github.com>
2018-06-03 16:23:03 -0400

    finished game reset in JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+      
 //Variables
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 12;
 var gameRunning = false;
 var words = ['strawberry', 'chocolate', 'mint', 'vanilla', 'pistachio'];
+var pickedWord = "";
+var pickedWordUnderscores = [];
+var lettersGuessedCorrect = [];
+var lettersGuessedWrong = [];
+
+//Reset Game
+function resetGame () {
+    gameRunning = "true"
+    guessesRemaining = 12;
+    lettersGuessedCorrect = [];
+    lettersGuessedWrong = [];
+    pickedWordUnderscores = [];
+    pickedWord = words[Math.floor(Math.random() + words.length)];
+    
+    for (var i = 0; i < pickedWord.length; i++) {
+        pickedWordUnderscores.push("_");
+    }
+
+    var guessesLeftElem = document.getElementById('guesses-left');
+    guessesLeftElem.textContent = guessesRemaining;
+
+    document.getElementById("gameReset").onkeydown = resetGame ();
+    
+};
+
+
+
+
+
+
 
 
 
