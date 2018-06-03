8d6435976fd36b7ab22f5088c57dcf275bca8836
Mike M <mikal815@users.noreply.github.com>
2018-06-03 14:24:00 -0400

    added JS variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     
+//Variables
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 12;
+var gameRunning = false;
+var words = ['strawberry', 'chocolate', 'mint', 'vanilla', 'pistachio'];
 
 
 
-
-
-
-
-
-
-
-
-
-    
 });
